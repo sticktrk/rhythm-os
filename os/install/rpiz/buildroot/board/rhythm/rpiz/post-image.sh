c50bc1098eb927dc81f14b8863598a0105fb5241
Tim K <3043754+sticktrk@users.noreply.github.com>
2026-04-13 21:03:14 -0400

    work

--- os/install/rpiz/buildroot/board/rhythm/rpiz/post-image.sh
+++ os/install/rpiz/buildroot/board/rhythm/rpiz/post-image.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
 BOARD_DIR="$(CDPATH= cd -- "$(dirname "$0")" && pwd)"
 BOOT_DIR="${BINARIES_DIR}/boot"
 TMP_DIR="${BUILD_DIR}/genimage.tmp"
+GENIMAGE_CFG="${BINARIES_DIR}/genimage-rpiz.cfg"
 
 rm -rf "${BOOT_DIR}" "${TMP_DIR}"
 mkdir -p "${BOOT_DIR}" "${TMP_DIR}"
@@ -21,9 +22,18 @@ if [ -f "${BINARIES_DIR}/bcm2708-rpi-zero-w.dtb" ]; then
     cp -f "${BINARIES_DIR}/bcm2708-rpi-zero-w.dtb" "${BOOT_DIR}/"
 fi
 
+FILES=()
+for i in "${BOOT_DIR}"/*; do
+    [ -e "${i}" ] || continue
+    FILES+=( "${i#${BINARIES_DIR}/}" )
+done
+
+BOOT_FILES=$(printf '\t\t\t"%s",\n' "${FILES[@]}")
+sed "s|#BOOT_FILES#|${BOOT_FILES}|" "${BOARD_DIR}/genimage.cfg" > "${GENIMAGE_CFG}"
+
 "${HOST_DIR}/bin/genimage" \
     --rootpath "${TARGET_DIR}" \
     --tmppath "${TMP_DIR}" \
     --inputpath "${BINARIES_DIR}" \
     --outputpath "${BINARIES_DIR}" \
-    --config "${BOARD_DIR}/genimage.cfg"
+    --config "${GENIMAGE_CFG}"
